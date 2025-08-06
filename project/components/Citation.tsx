88ba51e64db7d06e72955aec9f7edc45fd937ec7
Aaditya Baranwal <75058238+eternal-f1ame@users.noreply.github.com>
2025-08-06 00:21:12 -0400

    Update Citation.tsx

--- project/components/Citation.tsx
+++ project/components/Citation.tsx
@@ -8,7 +8,7 @@ export function Citation() {
   const bibtex = `@inproceedings{baranwal2025synspill,
     title={SynSpill: Improved Industrial Spill Detection With Synthetic Data},
     author={Baranwal, Aaditya and Mueez, Abdul and Voelker, Jason and Bhatia, Guneet and Vyas, Shruti},
-    booktitle={Proceedings of the IEEE/CVF International Conference on Computer Vision},
+    booktitle={Proceedings of the IEEE/CVF International Conference on Computer Vision - Workshops (ICCV-W)},
     year={2025}
 }`;
 
